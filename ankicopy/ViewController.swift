689962a59225c5fb0b34367eaed863f4f4ca9f90
Sergey Desenko <sergeydesenko@gmail.com>
2020-11-03 21:16:04 +0300

    Initial Commit

---
+++ ankicopy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ankicopy
+//
+//  Created by user on 03.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
